cbdff16cd823d14da278375a8d5ce3e5a82aeef1
tstrike <tstrike@users.noreply.github.com>
2022-09-20 00:54:10 -0400

    Add iowait warning and critical thresholds

--- check_cpu_used.sh
+++ check_cpu_used.sh
@@ -6,9 +6,9 @@
 #
 # Initial Author : Luke Harris
 # Adaptations by : tstrike, Etienne Magro
-# version       : 20140214
+# version       : 20220920
 # Creation date : 1 October 2010
-# Revision date : 12 May 2022
+# Revision date : 20 Sep 2022
 # Description   : Nagios plugin to check CPU performance statistics.
 #               This script has been tested on the following Linux and Unix platforms:
 #		RHEL 4, RHEL 5, RHEL 6, CentOS 4, CentOS 5, CentOS 6, SUSE, Ubuntu, Debian, FreeBSD 7, AIX 5, AIX 6, and Solaris 8 (Solaris 9 & 10 *should* work too)
@@ -20,44 +20,58 @@
 #		Support has been added for Nagios Plugin Performance Data for integration with Splunk, NagiosGrapher, PNP4Nagios,
 #		opcp, NagioStat, PerfParse, fifo-rrd, rrd-graph, etc
 #
-# USAGE         : ./check_cpu_perf.sh {warning} {critical}
+# USAGE         : ./check_cpu_perf.sh {warning} {critical} {iowait_warning} {iowait_critical}
 #
-# Example: ./check_cpu_perf.sh 80 90
+# Example: ./check_cpu_perf.sh 80 90 5 15
 # OK: CPU Idle = 84.10% | CpuUser=12.99; CpuNice=0.00; CpuSystem=2.90; CpuIowait=0.01; CpuSteal=0.00; CpuIdle=84.10:20:10
 #
 # Note: the option exists to NOT test for a threshold. Specifying 0 (zero) for both warning and critical will always return an exit code of 0.
 
+WARNING=${1:-0}
+CRITICAL=${2:-0}
+IOWAIT_WARNING=${3:-0}
+IOWAIT_CRITICAL=${4:-0}
 
 #Ensure warning and critical limits are passed as command-line arguments
 if [ -z "$1" -o -z "$2" ]
 then
- echo "Please include two arguments, eg."
- echo "Usage: $0 {warning} {critical}"
+ echo "Please include at least two arguments, eg."
+ echo "Usage: $0 {warning} {critical} [{iowait_warning} {iowait_critical}]"
  echo "Example :-"
  echo "$0 80 90"
 exit 3
 fi
 
 #Disable nagios alerts if warning and critical limits are both set to 0 (zero)
-if [ $1 -eq 0 ]
- then
-  if [ $2 -eq 0 ]
-   then
+if [ $WARNING -eq 0 -a $CRITICAL -eq 0 ]
+  then
     ALERT=false
-  fi
+fi
+
+#Disable nagios alerts if iowait_warning and iowait_critical limits are both set to 0 (zero)
+if [ $IOWAIT_WARNING -eq 0 -a $IOWAIT_CRITICAL -eq 0 ]
+  then
+    IOWAIT_ALERT=false
 fi
 
 #Ensure warning is greater than critical limit
-if [ $2 -lt $1 ]
+if [ $CRITICAL -lt $WARNING ]
  then
-  echo "Please ensure warning is greater than critical, eg."
+  echo "Please ensure critical threshold is greater than warning threshold, eg."
   echo "Usage: $0 80 90"
   exit 3
 fi
 
-SEUIL_WARN=$((100-$1))
-SEUIL_CRIT=$((100-$2))
+#Ensure iowait_warning is greater than iowait_critical limit
+if [ $IOWAIT_CRITICAL -lt $IOWAIT_WARNING ]
+ then
+  echo "Please ensure iowait_critical threshold is greater than iowait_warning threshold, eg."
+  echo "Usage: $0 80 90 5 15"
+  exit 3
+fi
 
+SEUIL_WARN=$((100-$WARNING))
+SEUIL_CRIT=$((100-$CRITICAL))
 
 #Detect which OS and if it is Linux then it will detect which Linux Distribution.
 OS=`uname -s`
@@ -141,10 +155,12 @@ SYSSTATRPM=`rpm -q sysstat|awk -F\- '{print $2}'|awk -F\. '{print $1}'`
 if [ $SYSSTATRPM -gt 5 ]
  then
   SARCPUIDLE=`echo ${SARCPU}|awk '{print $8}'|awk -F. '{print $1}'`
-  CPU=`echo ${SARCPU}|awk '{print "CPU Used = " 100-$8 "% | " "CpuUser=" $3 "; CpuNice=" $4 "; CpuSystem=" $5 "; CpuIowait=" $6 "; CpuSteal=" $7 "; CpuIdle=" $8";'$SEUIL_WARN';'$SEUIL_CRIT'"}'`
+  SARIOWAIT=`echo ${SARCPU}|awk '{print $6}'|awk -F. '{print $1}'`
+  CPU=`echo ${SARCPU}|awk '{print "CPU Used = " 100-$8 "% IOWAIT = " $6 "% | " "CpuUser=" $3 "; CpuNice=" $4 "; CpuSystem=" $5 "; CpuIowait=" $6 ";'$IOWAIT_WARNING';'$IOWAIT_CRITICAL' CpuSteal=" $7 "; CpuIdle=" $8";'$SEUIL_WARN';'$SEUIL_CRIT'"}'`
  else
   SARCPUIDLE=`echo ${SARCPU}|awk '{print $7}'|awk -F. '{print $1}'`
-  CPU=`echo ${SARCPU}|awk '{print "CPU Used = " 100-$7 "% | " "CpuUser=" $3 "; CpuNice=" $4 "; CpuSystem=" $5 "; CpuIowait=" $6 "; CpuIdle=" $7";'$SEUIL_WARN';'$SEUIL_CRIT'"}'`
+  SARIOWAIT=`echo ${SARCPU}|awk '{print $6}'|awk -F. '{print $1}'`
+  CPU=`echo ${SARCPU}|awk '{print "CPU Used = " 100-$7 "% IOWAIT = " $6 "% | " "CpuUser=" $3 "; CpuNice=" $4 "; CpuSystem=" $5 "; CpuIowait=" $6 ";'$IOWAIT_WARNING';'$IOWAIT_CRITICAL' CpuIdle=" $7";'$SEUIL_WARN';'$SEUIL_CRIT'"}'`
 fi
 ;;
 'dpkg')
@@ -153,10 +169,12 @@ SYSSTATDPKG=`dpkg -l sysstat|grep sysstat|awk '{print $3}'|awk -F\. '{print $1}'
 if [ $SYSSTATDPKG -gt 5 ]
  then
   SARCPUIDLE=`echo ${SARCPU}|awk '{print $8}'|awk -F. '{print $1}'`
-  CPU=`echo ${SARCPU}|awk '{print "CPU Used = " 100-$8 "% | " "CpuUser=" $3 "; CpuNice=" $4 "; CpuSystem=" $5 "; CpuIowait=" $6 "; CpuSteal=" $7 "; CpuIdle=" $8";'$SEUIL_WARN';'$SEUIL_CRIT'"}'`
+  SARIOWAIT=`echo ${SARCPU}|awk '{print $6}'|awk -F. '{print $1}'`
+  CPU=`echo ${SARCPU}|awk '{print "CPU Used = " 100-$8 "% IOWAIT = " $6 "% | " "CpuUser=" $3 "; CpuNice=" $4 "; CpuSystem=" $5 "; CpuIowait=" $6 ";'$IOWAIT_WARNING';'$IOWAIT_CRITICAL' CpuSteal=" $7 "; CpuIdle=" $8";'$SEUIL_WARN';'$SEUIL_CRIT'"}'`
  else
   SARCPUIDLE=`echo ${SARCPU}|awk '{print $7}'|awk -F. '{print $1}'`
-  CPU=`echo ${SARCPU}|awk '{print "CPU Used = " 100-$7 "% | " "CpuUser=" $3 "; CpuNice=" $4 "; CpuSystem=" $5 "; CpuIowait=" $6 "; CpuIdle=" $7";'$SEUIL_WARN';'$SEUIL_CRIT'"}'`
+  SARIOWAIT=`echo ${SARCPU}|awk '{print $6}'|awk -F. '{print $1}'`
+  CPU=`echo ${SARCPU}|awk '{print "CPU Used = " 100-$7 "% IOWAIT = " $6 "% | " "CpuUser=" $3 "; CpuNice=" $4 "; CpuSystem=" $5 "; CpuIowait=" $6 ";'$IOWAIT_WARNING';'$IOWAIT_CRITICAL' CpuIdle=" $7";'$SEUIL_WARN';'$SEUIL_CRIT'"}'`
 fi
 ;;
 'lslpp')
@@ -167,8 +185,9 @@ if [ $SYSSTATLSLPP -gt 4 ]
   CpuPhysc=`echo ${SARCPU}|awk '{print $6}'`
   LPARCPU=`/usr/bin/lparstat -i | grep "Maximum Capacity" | awk '{print $4}' |head -1`
   SARCPUIDLE=`echo "scale=2;100-(${CpuPhysc}/${LPARCPU}*100)" | bc | awk -F. '{print $1}'`
-  PERFDATA=`echo ${SARCPU}|awk '{print "CpuUser=" $2 "; CpuSystem=" $3 "; CpuIowait=" $4 "; CpuPhysc=" $6 "; CpuEntc=" $7 "; CpuIdle=" $5";'$SEUIL_WARN';'$SEUIL_CRIT'"}'`
-  CPU=`echo "CPU Idle = "${SARCPUIDLE}"% |" ${PERFDATA}"; LparCpuIdle="${SARCPUIDLE}"; LparCpuTotal="$LPARCPU`
+  SARIOWAIT=`echo ${SARCPU}|awk '{print $4}'|awk -F. '{print $1}'`
+  PERFDATA=`echo ${SARCPU}|awk '{print "CpuUser=" $2 "; CpuSystem=" $3 "; CpuIowait=" $4 ";'$IOWAIT_WARNING';'$IOWAIT_CRITICAL' CpuPhysc=" $6 "; CpuEntc=" $7 "; CpuIdle=" $5";'$SEUIL_WARN';'$SEUIL_CRIT'"}'`
+  CPU=`echo "CPU Idle = "${SARCPUIDLE}"% IOWAIT = "${SARIOWAIT}"%|" ${PERFDATA}"; LparCpuIdle="${SARCPUIDLE}"; LparCpuTotal="$LPARCPU`
  else
   echo "AIX $SYSSTATLSLPP Not Supported"
   exit 3
@@ -180,7 +199,8 @@ SYSSTATPKGINFO=`pkginfo -l SUNWaccu|grep VERSION|awk '{print $2}'|awk -F\. '{pri
 if [ $SYSSTATPKGINFO -ge 11 ]
  then
   SARCPUIDLE=`echo ${SARCPU}|awk '{print $5}'`
-  CPU=`echo ${SARCPU}|awk '{print "CPU Used = " 100-$5 "% | " "CpuUser=" $2 "; CpuSystem=" $3 "; CpuIowait=" $4 "; CpuIdle=" $5";'$SEUIL_WARN';'$SEUIL_CRIT'"}'`
+  SARIOWAIT=`echo ${SARCPU}|awk '{print $4}'|awk -F. '{print $1}'`
+  CPU=`echo ${SARCPU}|awk '{print "CPU Used = " 100-$5 "% IOWAIT = " $4 "% | " "CpuUser=" $2 "; CpuSystem=" $3 "; CpuIowait=" $4 ";'$IOWAIT_WARNING';'$IOWAIT_CRITICAL' CpuIdle=" $5";'$SEUIL_WARN';'$SEUIL_CRIT'"}'`
  else
   echo "Solaris $SYSSTATPKGINFO Not Supported"
   exit 3
@@ -192,6 +212,7 @@ SYSSTATPKGINFO=`pkg_info | grep ^bsdsar | awk -F\- '{print $2}' | awk -F\. '{pri
 if [ $SYSSTATPKGINFO -ge 1 ]
  then
   SARCPUIDLE=`echo ${SARCPU}|awk '{print $6}'`
+  SARIOWAIT=0
   CPU=`echo ${SARCPU}|awk '{print "CPU Used = " 100-$6 "% | " "CpuUser=" $2 "; CpuSystem=" $3 "; CpuNice=" $4 "; CpuIntrpt=" $5 "; CpuIdle=" $6";'$SEUIL_WARN';'$SEUIL_CRIT'""}'`
  else
   echo "BSD $SYSSTATPKGINFO Not Supported"
@@ -201,20 +222,21 @@ fi
 esac
 
 #Display CPU Performance without alert
-if [ "$ALERT" == "false" ]
+if [ "$ALERT" == "false" -a "$IOWAIT_ALERT" == "false" ]
  then
-		echo "$CPU"
-		exit 0
+	echo "$CPU"
+	exit 0
  else
         ALERT=true
+	IOWAIT_ALERT=true
 fi
 
 #Display CPU Performance with alert
-if [ ${SARCPUIDLE} -lt ${SEUIL_CRIT} ]
+if [ ${SARCPUIDLE} -lt ${SEUIL_CRIT} -o ${SARIOWAIT} -gt ${IOWAIT_CRITICAL} ]
  then
 		echo "CRITICAL: $CPU"
 		exit 2
- elif [ ${SARCPUIDLE} -lt ${SEUIL_WARN} ]
+ elif [ ${SARCPUIDLE} -lt ${SEUIL_WARN} -o ${SARIOWAIT} -gt ${IOWAIT_WARNING} ]
 		 then
 		  echo "WARNING: $CPU"
 		  exit 1
